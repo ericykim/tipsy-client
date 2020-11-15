7d7d4d11afa90c9721653b67c1feb158e6a6e4ec
Mckenna Poulos <poulos.m@husky.neu.edu>
2020-11-15 17:22:17 -0500

    add fonts and style hover

--- src/styles/typeStyles.js
+++ src/styles/typeStyles.js
@@ -1,26 +1,57 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import { SITE_HIGHLIGHT, SITE_RED, TEXT_BLACK } from "./colors";
 
 export const H1 = styled.h1`
-    font-size: 55px;
-    color: pink;
-`
+  font-family: FormulaCondensed-Black, sans-serif;
+  font-weight: 900;
+  font-style: normal;
+
+  font-size: 75px;
+  letter-spacing: 5px;
+  color: transparent;
+  -webkit-text-stroke: 2px ${TEXT_BLACK};
+  white-space: nowrap;
+
+  @media (min-width: 576px) {
+    font-size: 125px;
+    -webkit-text-stroke: 3px ${TEXT_BLACK};
+  }
+
+  :hover {
+    -webkit-text-stroke: 2px ${SITE_HIGHLIGHT};
+    color: ${TEXT_BLACK};
+    cursor: pointer;
+    text-shadow: -3.5px 4px ${TEXT_BLACK};
+  }
+`;
 
 export const H2 = styled.h2`
-    font-size: 32px;
-    color: orange;
-`
+  font-family: FormulaCondensed-Regular, sans-serif;
+  font-weight: 400;
+  font-style: normal;
+
+  font-size: 50px;
+  letter-spacing: 2px;
+  color: ${SITE_RED};
+`;
 
 export const H3 = styled.h3`
-    font-size: 20px;
-    color: purple;
-`
+  font-size: 20px;
+  color: purple;
+`;
 
 export const Label = styled.p`
-    font-size: 20px;
-    color: blue;
-`
+  font-size: 20px;
+  color: blue;
+`;
+
+export const P1 = styled.p`
+  font-family: montserrat, sans-serif;
+  font-weight: 400;
+  font-style: normal;
+`;
 
 export const SmallText = styled.p`
-    font-size: 10px;
-    color: gray;
-`
+  font-size: 10px;
+  color: gray;
+`;
